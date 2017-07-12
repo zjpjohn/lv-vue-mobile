058b5478777b8bfa89bf167e8a4a6e9d1a23735b
[joshinrai] <[joshinrai@163.com]>
2017-07-12 17:58:58 +0800

    构建vue-mobile项目
    构建绿城网上商城vue-mobile项目

---
+++ src/pages/login/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <span>{{text}}</span>
+  </div>
+</template>
+<script>
+  export default {
+    components: {
+      
+    },
+    data() {
+      return {
+        showtoasttext:'',
+        showPositionValue:false,
+        isGetcode: false,
+        phoneNumber: '',
+        verificationcode: '',
+        codecolor: 'white',
+        type: 0,
+        text: '登录',
+        isLogin: false,
+        time: 60,
+        show: true,
+        openid: ""
+      }
+    },
+    mounted() {
+
+    },
+    methods: {
+      //获取验证码
+      getverificationcode () {
+        
+      }
+    }
+  }
+</script>
+<style scoped>
+</style>
