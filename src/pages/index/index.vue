1d8b91fc3f0ea634d098288472c08f6274c9fd49
[joshinrai] <[joshinrai@163.com]>
2017-07-13 18:00:32 +0800

    提交代码
    提交代码

---
+++ src/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="index-container">
+      <h1 class="index-title">{{text}}</h1>
+      
+  </div>
+</template>
+<script>
+  import { Tab, TabItem, Swiper, SwiperItem, XInput, Group, XButton, Toast, XHeader, Search, Countdown } from 'vux'
+  export default {
+    components: {
+      Tab, TabItem, Swiper, SwiperItem, XInput, Group, Toast, XHeader, Search, Countdown,Toast
+    },
+    data() {
+      return {
+        text: '商城',
+      }
+    },
+    mounted() {
+
+    },
+    methods: {
+      //获取验证码
+      getverificationcode () {
+        
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  @import '~vux/src/styles/1px.less';
+  @import '~vux/src/styles/center.less';
+  * { touch-action: none; }
+  .index-title{
+    position:absolute;
+    top:0;
+    width:100vw;
+    font-family:'微软雅黑 Regular', '微软雅黑';
+    font-weight:400;
+    font-style:normal;
+    font-size:18px;
+    color:#993333;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/pages/login/login'
+import Index from '@/pages/index/index'
 
 Vue.use(Router)
 
@@ -16,6 +17,10 @@ export default new Router({
     {
       path: '/login', // 登录
       component: Login
+    },
+    {
+      path: '/Index', // 登录
+      component: Index
     }
   ]
 })
