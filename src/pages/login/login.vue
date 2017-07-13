bd4307543392f0220f14e1ee0f55fab7db50396a
[joshinrai] <[joshinrai@163.com]>
2017-07-13 17:10:05 +0800

    login页
    login页

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -2,7 +2,13 @@
   <div class="login-container">
       <h1 class="login-title">{{text}}</h1>
       <img class="login-top-img" src="~img/u271.jpg" alt="登录图片">
-      <x-input class="phone_record" name="mobile" :max="11" is-type="china-mobile" v-model="phoneNumber" placeholder="请输入您的手机号登录">测试</x-input>
+      <group class="verificationcode">
+        <x-input class="phone_record" name="mobile" :max="11" is-type="china-mobile" v-model="phoneNumber" placeholder="请输入手机号"/>
+        <x-input  class="verificationcode_hint" :max="4" v-model ="verificationcode" placeholder="请输入验证码">
+          <button slot="right" v-model="type" v-if="true" class="recode">获取验证码</button>
+        </x-input>
+      </group>
+      <button class="login">{{text}}</button>
   </div>
 </template>
 <script>
@@ -54,12 +60,40 @@
     width:100vw;
     height:10.5rem;
   }
+  .verificationcode{
+    width:90vw;
+    margin-left:5vw;
+  }
   .phone_record{
     border-radius: 3px;
     font-size: 0.32rem;
     color: #999999;
-    height: 1rem;
+    height: 3rem;
     margin: 0 0.46rem;
     padding:0 0.46rem;
+  }
+  .verificationcode_hint{
+    font-size: 0.32rem;
+  }
+  .recode{
+    outline: none;
+    font-size: 0.28rem;
+    color: #71A061;
+    height: 1.7rem;
+    width: 4.86rem;
+    border:solid 1px  #71a061;
+    border-radius: 2px;
+    background: white;
+    margin-right: -0.1rem;
+  }
+  .login{
+    border: none;
+    background: #71a061;
+    font-size: 0.4rem;
+    color: #ffffff;
+    border-radius: 2px;
+    height: 3rem;
+    margin: 0 0.46rem;
+    width: 90vw;
   }
 </style>
